2c44b90a1ebf47095be9f298573404f161674ef6
Rocco Minetola <Rocco.Minetola@MyGeorgian.ca>
2026-08-11 13:36:45 -0400

    Adding in the spotify info I need (I will add the IDs in when uploading to webserver)

--- js/script.js
+++ js/script.js
@@ -6,3 +6,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const studentInfoEl = document.getElementById("student-info");
   studentInfoEl.textContent = `Student ID: ${studentId} / Name: ${studentName}`;
 });
+
+/* Spotify Info */
+/* For now i'm leaving these blank because I do not want to upload them to Github but will add them later on when I upload to a web server */
+const CLIENT_ID = "Temp";
+const CLIENT_SECRET = "Temp-Secret"
+const DRAKE_ARTIST_ID = "3TVXtAsR1Inumwj472S9r4";
